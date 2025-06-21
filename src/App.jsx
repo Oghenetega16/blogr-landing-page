7293994648a72f48f53ef938d90767a600e2102d
oghenetega16 <sukuruoghenetega@gmail.com>
2025-06-21 23:57:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/navbar";
+
+
+export default function App() {
+    return (
+      <main className="min-h-screen bg-red-300">
+        <NavBar />
+      </main>
+    )
+}
+
---
+++ src/components/navbar.jsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <img src="/assets/images/logo.svg" alt="logo" />
+        </nav>
+    )
+}
